0273f02251f9c48defd243675d36f701b026239f
Conor Kemp <conorkemp@gmail.com>
2022-03-25 20:06:14 +0000

    Added Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { db } from './firebase.config';
 import { collection, doc, getDocs } from "firebase/firestore";
-import SimpleDateTime  from 'react-simple-timestamp-to-date';
+import SimpleDateTime from 'react-simple-timestamp-to-date';
 import './App.css';
 
 function App() {
@@ -11,18 +11,20 @@ function App() {
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(blogCollectionRef);
-      setPosts(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
+      setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
     };
     getPosts();
-  },[]);
+  }, []);
   return (
 
     <div className="App">
       {posts.map((post => {
         return (
-          <div key={post.id}><h1>{post.title}</h1>
-          <span>Posted: </span><SimpleDateTime dateFormat="DMY" dateSeparator="/"  timeSeparator=":">{post.timestamp}</SimpleDateTime>
-          <h3>{post.content}</h3></div>
+          <div key={post.id}>
+            <h1 className='font-bold underline'>{post.title}</h1>
+            <span>Posted: </span><SimpleDateTime dateFormat="DMY" dateSeparator="/" timeSeparator=":">{post.timestamp}</SimpleDateTime>
+            <h3>{post.content}</h3>
+          </div>
         )
       }))}
     </div>
