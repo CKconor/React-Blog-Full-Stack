2eba7d6632768fe10f983820ffb08b58354abc5c
Conor Kemp <conorkemp@gmail.com>
2022-03-28 18:23:43 +0100

    Added content to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div className="flex flex-row mt-10"><h1 className="text-5xl font-bold mb-0 mt-0">
-    About Me
-  </h1></div>
-  )
+    <div className="flex flex-col mt-10 max-w-2xl">
+      <h1 className="text-5xl font-bold mb-10 mt-0">About Me</h1>
+      <h2 className="font-bold text-4xl mb-2">Job Title</h2>
+      <p>Software Engineer & Frontend Developer</p>
+      <h2 className="font-bold text-4xl mb-2 mt-6">Experience</h2>
+      <p className="mb-4">
+        I have worked as a Frontend Developer for the past 3 years after
+        teaching myself coding during the pandemic after being furloughed and
+        bored. I love all things design and coding with a desire to really push
+        the boat out with designs and layouts.
+      </p>
+      <p>
+        Specialising in React Webpages and PHP driven to CMS's I truly believe I
+        can make you not only a beautiful website but also an extremely
+        efficient one.
+      </p>
+      <h2 className="font-bold text-4xl mb-2 mt-6">Work History</h2>
+    </div>
+  );
 }
 
-export default About
+export default About;
