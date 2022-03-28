756e89c76a3e0a5e0c0e1dba3fb728dd17e83a76
Conor Kemp <conorkemp@gmail.com>
2022-03-28 18:33:25 +0100

    Added animation to skills filter tab

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,9 +6,9 @@ function Skills() {
     <div className="skills-container">
       <div className="flex flex-row mb-2 filter-row">
         <a className="mr-10 active hover:text-darkmode">All</a>
-        <a className="mr-10 text-lightsubtext hover:text-darkmode">Developing</a>
-        <a className="mr-10 text-lightsubtext hover:text-darkmode">Design</a>
-        <a className="text-lightsubtext hover:text-darkmode">Business</a>
+        <a className="mr-10 text-lightsubtext hover:text-darkmode hover:font-bold">Developing</a>
+        <a className="mr-10 text-lightsubtext hover:text-darkmode hover:font-bold">Design</a>
+        <a className="text-lightsubtext hover:text-darkmode hover:font-bold">Business</a>
       </div>
       <div className="flex flex-row bg-skillcontainerlightbg dark:bg-skillcontainerdarkbg p-10 rounded text-lightsubtext dark:text-darksubtext">
         <div className="flex-col basis-1/3">
