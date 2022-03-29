53f9a38db319dc2a29047259bffa7e0db816845f
Conor Kemp <conorkemp@gmail.com>
2022-03-29 11:29:38 +0100

    Fixed dark mode hover on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,16 @@ function Header() {
   return (
       <div className="mb-5 mt-5">
         <nav className="flex flex-row">
-          <NavLink to={"/"} className="mr-10 text-lightsubtext hover:text-darkmode hover:font-bold transition-all">
+          <NavLink to={"/"} className="mr-10 text-lightsubtext dark:hover:text-lightmode hover:text-darkmode hover:font-bold transition-all">
             Home
           </NavLink>
-          <NavLink to={"/about"} className="mr-10 text-lightsubtext hover:text-darkmode hover:font-bold transition-all">
+          <NavLink to={"/about"} className="mr-10 text-lightsubtext dark:hover:text-lightmode hover:text-darkmode hover:font-bold transition-all">
             About
           </NavLink>
-          <NavLink to={"/projects"} className="mr-10 text-lightsubtext hover:text-darkmode hover:font-bold transition-all">
+          <NavLink to={"/projects"} className="mr-10 text-lightsubtext dark:hover:text-lightmode hover:text-darkmode hover:font-bold transition-all">
             Projects
           </NavLink>
-          <NavLink to={"/blog"} className="mr-10 text-lightsubtext hover:text-darkmode hover:font-bold transition-all">
+          <NavLink to={"/blog"} className="mr-10 text-lightsubtext dark:hover:text-lightmode hover:text-darkmode hover:font-bold transition-all">
             Blog
           </NavLink>
         </nav>
