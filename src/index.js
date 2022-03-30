ab792c63e81fd205d27f956c5dd4ae21abd35d05
Conor Kemp <conorkemp@gmail.com>
2022-03-30 19:27:37 +0100

    Linked Contentful to GraphQL and setup blog api

--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: `https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}`,
+  cache: new InMemoryCache(),
+  headers: {
+    'Authorization': `Bearer ${process.env.REACT_APP_CONTENTFUL_AUTH_KEY}`
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
-import { useState, useEffect } from "react";
+import {useQuery, gql} from '@apollo/client'
+import ReactMarkdown from 'react-markdown'
 
+const GET_BLOGS = gql`
+  query Blogs {
+    blogPostCollection{
+      items {
+       blogTitle
+        sys {
+  id}
+        featureimage {
+          title
+          url
+        }
+        blogContent
+      }
+    }
+  }
+  `
 function Blogs() {
+  const { loading, error, data } = useQuery(GET_BLOGS);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
   
   return (
     <div className="flex flex-col mt-10"><h1 className="text-5xl font-bold mb-0 mt-0">Blog</h1>
+      {data.blogPostCollection.items.map (blog => {
+        return (
+          <div className="" key={blog.sys.id}>
+          <h1>{blog.blogTitle}</h1>
+          {blog.featureimage && 
+          <img src={blog.featureimage.url} alt={blog.featureimage.title} />
+          }
+          <div><ReactMarkdown>{blog.blogContent}</ReactMarkdown></div>
+          </div>
+        )
+      })}
     </div>
   )
 }
