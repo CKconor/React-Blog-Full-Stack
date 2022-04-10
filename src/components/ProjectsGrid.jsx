357ef5075f5ce82ccc7fb31e3eba20fcdf6ed6fe
Conor Kemp <conorkemp@gmail.com>
2022-04-10 17:33:22 +0100

    Styled project grid and added placeholders

--- src/components/ProjectsGrid.jsx
+++ src/components/ProjectsGrid.jsx
@@ -2,13 +2,43 @@ import React from 'react'
 
 function ProjectsGrid() {
   return (
-    <div className="flex flex-row">
-      <div className="project-card flex flex-col basis-1/2">
-        <h1>Test</h1>
+    <div>
+    <div className="flex flex-row justify-between max-w-[90%]">
+    <div className="project-card flex flex-col basis-2/5 border-4 dark:border-lightmode border-darkmode rounded-lg px-8 py-6">
+      <h3 className="font-bold text-2xl mb-1 mt-0">Gtech</h3>
+      <p className="max-w-prose mt-2 text-lightsubtext dark:text-darksubtext">
+              Web Developer and Designer based in the UK, specialising
+              in e-commerce and business branding alongside modern frameworks
+              such as React and CMS systems such as Wordpress
+            </p>
       </div>
-      <div className="project-card flex flex-col basis-1/2">
-        <h1>Test</h1>
+      <div className="project-card flex flex-col basis-2/5 border-4 dark:border-lightmode border-darkmode rounded-lg px-8 py-6">
+      <h3 className="font-bold text-2xl mb-1 mt-0">FM Streamer Showdown</h3>
+      <p className="max-w-prose mt-2 text-lightsubtext dark:text-darksubtext">
+              Web Developer and Designer based in the UK, specialising
+              in e-commerce and business branding alongside modern frameworks
+              such as React and CMS systems such as Wordpress
+            </p>
       </div>
+      </div>
+      <div className="flex flex-row justify-between max-w-[90%] mt-6">
+      <div className="project-card flex flex-col basis-2/5 border-4 dark:border-lightmode border-darkmode rounded-lg px-8 py-6">
+      <h3 className="font-bold text-2xl mb-1 mt-0">Oldcastle Weddings</h3>
+      <p className="max-w-prose mt-2 text-lightsubtext dark:text-darksubtext">
+              Web Developer and Designer based in the UK, specialising
+              in e-commerce and business branding alongside modern frameworks
+              such as React and CMS systems such as Wordpress
+            </p>
+      </div>
+      <div className="project-card flex flex-col basis-2/5 border-4 dark:border-lightmode border-darkmode rounded-lg px-8 py-6">
+      <h3 className="font-bold text-2xl mb-1 mt-0">Chics LTD</h3>
+      <p className="max-w-prose mt-2 text-lightsubtext dark:text-darksubtext">
+              Web Developer and Designer based in the UK, specialising
+              in e-commerce and business branding alongside modern frameworks
+              such as React and CMS systems such as Wordpress
+            </p>
+      </div>
+    </div>
     </div>
   )
 }
