59c80d378c3e23754937f3644d00cc0041826286
Conor Kemp <conorkemp@gmail.com>
2022-12-20 14:52:08 +0000

    Removed dupe

--- src/components/ProjectsGrid.jsx
+++ src/components/ProjectsGrid.jsx
@@ -35,7 +35,7 @@ function ProjectsGrid() {
               key={project.sys.id}
               to={`/projects/${project.projectSlug}`}
               state={{ projectData: project }}
-              className="cursor-pointer project-card sm:mb-0 w-full md:w-auto mb-4 cursor-pointer hover:scale-105 transition-all min-h-[210px] mr-8 flex flex-col basis-1/3 border-4 dark:border-lightmode border-darkmode rounded-lg px-8 py-6"
+              className="cursor-pointer project-card sm:mb-0 w-full md:w-auto mb-4  hover:scale-105 transition-all min-h-[210px] mr-8 flex flex-col basis-1/3 border-4 dark:border-lightmode border-darkmode rounded-lg px-8 py-6"
             >
               <div>
                 <h3 className="font-bold text-2xl mb-1 mt-0">
