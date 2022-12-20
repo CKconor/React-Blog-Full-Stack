625a98509fb88b30a23509effdac572596130cc6
Conor Kemp <conorkemp@gmail.com>
2022-12-20 16:27:35 +0000

    Added hover state

--- src/components/ExperienceGrid.jsx
+++ src/components/ExperienceGrid.jsx
@@ -45,7 +45,7 @@ function ExperienceGrid() {
   return (
     <div>
       {experienceList.map((experience) => (
-        <div className="flex flex-row flex-1 border-2 border-skillcontainerlighbg dark:border-skillcontainerdarkbg flex-wrap p-8 items-center rounded mb-8">
+        <div className="hover:scale-105 transition-all flex flex-row flex-1 border-2 border-skillcontainerlighbg dark:border-skillcontainerdarkbg flex-wrap p-8 items-center rounded mb-8">
           <div className="flex flex-col">
             <h2 className="mb-1">{experience.company}</h2>
             <div className="flex flex-row flex-1 flex-wrap">
