7cea8a8be16a114e26dbe60375228276c2b37ca5
Conor Kemp <conorkemp@gmail.com>
2024-01-30 14:39:26 +0000

    Fix mailto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Contact(className) {
   return (
     <div className="rounded border border-accentcolor bg-accentcolor px-4 py-2 text-lightmode transition duration-0 hover:bg-transparent hover:text-accentcolor hover:duration-150 dark:text-darkmode">
-      <a href="mailtoconorkempwebstudio@gmail.com">
+      <a href="mailto:conorkempwebstudio@gmail.com">
         <button className="font-bold">Contact Me</button>
       </a>
     </div>
