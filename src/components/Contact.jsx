89e222fc978259579a22ed2449133de9d200f65c
Conor Kemp <conorkemp@gmail.com>
2024-01-30 14:37:53 +0000

    New email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Contact(className) {
   return (
     <div className="rounded border border-accentcolor bg-accentcolor px-4 py-2 text-lightmode transition duration-0 hover:bg-transparent hover:text-accentcolor hover:duration-150 dark:text-darkmode">
-      <a href="mailto:conorkempmedia@gmail.com">
+      <a href="mailtoconorkempwebstudio@gmail.com">
         <button className="font-bold">Contact Me</button>
       </a>
     </div>
